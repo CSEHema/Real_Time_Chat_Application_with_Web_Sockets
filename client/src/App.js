4241f75e781979ae6c25732ba254b10e04d28f5c
Mandalemula Hema Siri Ramya <hemasiri1405@gmail.com>
2025-12-24 18:23:35 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,33 @@
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import './App.css';
 
+import Login from './pages/Login.js';
+import Registration from './pages/Registration.js';
+import Dashboard from './pages/Dashboard.js';
+
+import { SocketProvider } from './context/SocketContext';
+function App() {
+  const getStoredUser = () => {
+    const user = localStorage.getItem("user");
+    return user ? JSON.parse(user) : null;
+  };
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} ></Route>
+        <Route path='/signup' element={<Registration/>}></Route>
+        <Route 
+          path='/dashboard' 
+          element={
+            <SocketProvider currentUser={getStoredUser()}>
+              <Dashboard />
+            </SocketProvider>
+          } 
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
